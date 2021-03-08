d33ac37d8e038fb5a29cbf22aace275d0df325ca
Bradley Dion <bradleydion@MacBook-Pro.local>
2021-03-07 20:43:11 -0500

    started my server and getting ready to move to the next steps

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
 // build your server here and require it from index.js
+const express = require("express")
+const helmet = require("helmet")
+
+
+const server = express()
+server.use(helmet())
+server.use(express.json())
+
+
+server.use((req,res,next)=>{
+    res.json({message:"Welcome to the 4.2 sprint challenge, may the odds be ever in your favor."})
+})
+
+
+server.use((err,req,res,next)=>{
+    console.log(err)
+    res.status(500).json({message:"Molly! You in danger girl!!"})
+})
